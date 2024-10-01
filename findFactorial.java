264597aa3b1846344840a17d0dbd326afd21f62f
neeraj3161 <neerajtripathi7447@gmail.com>
2024-10-01 17:05:34 +0530

    Find factorial of any number using java.

--- findFactorial.java
+++ findFactorial.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class findFactorial {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Scanner sc = new Scanner(System.in);
+        int exit = 0;
+        while(exit == 0){
+        
+            System.out.print("Enter a number: ");
+            int number = sc.nextInt();
+            int f = 1;
+
+            for(int i = number; i>=1; i--){
+                f = f*i;
+            }
+            System.out.println("The factorial of given number is: " + f);
+            
+            System.out.print("To continue again press 0 or to exit press 1: ");
+            exit = sc.nextInt();
+        }
     }
 }
